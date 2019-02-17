59f414c64f4db4e631dfc9e62954be62eadb6caf
andrewcooke89 <36635993+andrewcooke89@users.noreply.github.com>
2019-02-17 10:17:29 +0000

    Updates

---
+++ precache-manifest.e14bd245e5e36017fea2ea36dddc88a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8619ffea42218cb0ceac",
+    "url": "/robotFriends/static/js/runtime~main.8619ffea.js"
+  },
+  {
+    "revision": "cf63658dd580d769c82c",
+    "url": "/robotFriends/static/js/main.cf63658d.chunk.js"
+  },
+  {
+    "revision": "5bca8c53d5c0fad68a47",
+    "url": "/robotFriends/static/js/1.5bca8c53.chunk.js"
+  },
+  {
+    "revision": "cf63658dd580d769c82c",
+    "url": "/robotFriends/static/css/main.180c57bd.chunk.css"
+  },
+  {
+    "revision": "5bca8c53d5c0fad68a47",
+    "url": "/robotFriends/static/css/1.cd403938.chunk.css"
+  },
+  {
+    "revision": "aec0aa8da4e7dc8ace94842bd1ed65ef",
+    "url": "/robotFriends/index.html"
+  }
+];
